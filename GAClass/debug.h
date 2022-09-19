7ee66a44dc1923854822c0eb716be7d6ed1f581d
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2022-09-19 16:45:32 -0400

    hw 1 changes to comments (debug.h)

--- GAClass/debug.h
+++ GAClass/debug.h
@@ -14,9 +14,24 @@ typedef enum debug_print_t {
 	k_print_error = 1 << 2
 } debug_print_t;
 
+// Flags for debug_print().
 static LONG debug_exception_handler(LPEXCEPTION_POINTERS POINTER);
+
+// Install unhandled exception handler.
+// When unhandled exceptions are caught, will log an error and capture a memory dump.
 void debug_install_exception_handler();
+
+// Set mask of which types of prints will actually fire.
+// See the debug_print().
 void debug_set_print_mask(uint32_t mask);
+
+// Log a message to the console.
+// Message may be dropped if type is not in the active mask.
+// See debug_set_print_mask.
 void debug_print_line(uint32_t type, _Printf_format_string_ const char* format, ...);
+
+// Given an allocation node with a stack backtrace, prints out the memory leak 
+// from the memory given
+void debug_backtrace(alloc_node_t* allocation_node);
 
 #endif
